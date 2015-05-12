866939030a5b0ab308d06e11b1a300b4eec06eb2
ramesh-git-test <rramesh.aut@gmail.com>
2015-05-12 12:31:33 +0530

    13

--- sampleFinal2/Program.cs
+++ sampleFinal2/Program.cs
@@ -9,7 +9,7 @@ namespace sampleFinal2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World binary files moved to another location4 auto commit 12.....!");
+            Console.WriteLine("Hello World binary files moved to another location4 auto commit 13.....!");
             Console.ReadKey();
         }
     }
