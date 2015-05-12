1abb394bfc561772c8c2e2f658c36635c8a39b8c
ramesh-git-test <rramesh.aut@gmail.com>
2015-05-12 11:46:46 +0530

    first

---
+++ sampleFinal2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sampleFinal2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World binary files moved to another location4 auto commit 4.....!");
+            Console.ReadKey();
+        }
+    }
+}
